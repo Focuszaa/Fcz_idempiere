1fdc0908158929cb4f056ae2f7c25f315781bf7e
nirut phengjaiwong <nirut.phengjaiwong@zuelligindustrial.com>
2018-10-12 11:43:05 +0700

    Delete source code for replacing ZI source code 2

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/validator/WindowValidator.java
+++
@@ -1,7 +0,0 @@
-package org.adempiere.webui.adwindow.validator;
-
-import org.adempiere.util.Callback;
-
-public interface WindowValidator {
-	public void onWindowEvent(WindowValidatorEvent event, Callback<Boolean> callback);
-}
